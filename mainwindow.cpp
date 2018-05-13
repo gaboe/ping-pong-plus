9d064692336f4a1293c75a53ee95444e93caf96c
gaboe <gabrielecegi@gmail.com>
2018-05-13 15:45:38 +0200

    move player on keypress

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QKeyEvent>
+#include <QtDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -18,12 +19,12 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
 {
     if( event->key() == Qt::Key_Down )
     {
-        qDebug("dule");
+        ui->playerOne->move(ui->playerOne->pos().x(),ui->playerOne->pos().y() + 20);
 
     }
     if( event->key() == Qt::Key_Up )
     {
-        qDebug("hure");
+        ui->playerOne->move(ui->playerOne->pos().x(),ui->playerOne->pos().y() - 20);
     }
 }
 
